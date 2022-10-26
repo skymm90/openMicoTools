fc057f687507a51dd66d1885597673a1b6e96fbe
skymm90 <52227414+skymm90@users.noreply.github.com>
2022-10-26 18:18:53 +0800

    Create testwin.cc

---
+++ testwin.cc
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+#include <string>
+#include <unistd.h>
+using namespace std;
+ 
+ 
+
+int main (int argc, char *argv [])
+{
+     printf("helo word");
+   
+}
