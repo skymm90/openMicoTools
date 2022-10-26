ed6931a45d2b68c7c352d7822a5fff6d17e08683
skymm90 <52227414+skymm90@users.noreply.github.com>
2022-10-26 18:36:31 +0800

    Update testwin.cc

--- testwin.cc
+++ testwin.cc
@@ -1,15 +1,13 @@
-#include <fstream>
-#include <iostream>
+ 
 #include <stdio.h>
-#include <iomanip>
+ 
 #include <string>
 #include <unistd.h>
 using namespace std;
- 
- 
 
+ 
 int main (int argc, char *argv [])
 {
-     printf("helo word");
+    printf("pre start");
    
 }
